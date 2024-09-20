34ccff28fe6378cedf6bc1c0ff82f54d2c17122d
Millyae <i.esargsyan@yandex.ru>
2024-09-20 10:33:25 +0300

    Спринт 1

---
+++ project/pereval/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Users, Coords, PerevalAdded, PerevalImage
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ['email', 'phone', 'fam', 'name', 'otc']
+
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = ['latitude', 'longitude', 'height']
+
+class PerevalImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PerevalImage
+        fields = ['img', 'title']
+
+class PerevalAddedSerializer(serializers.ModelSerializer):
+    coord = CoordsSerializer()
+    images = PerevalImageSerializer(many=True, read_only=True)  # Добавлено для отображения изображений
+
+    class Meta:
+        model = PerevalAdded
+        fields = ['date_added', 'beauty_title', 'title', 'other_titles', 'connect', 'add_time', 'coord', 'level_winter', 'level_summer', 'level_autumn', 'level_spring', 'images']
